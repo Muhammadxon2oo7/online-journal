ccfa23df2598f8a11c9618a91441b29f66687612
Muhammadxon_2oo7 <muhammadxon0709@gmail.com>
2023-03-22 23:09:00 +0300

    first commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,34 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import c from "./Login.module.scss"
 import logo from "../../Assets/img/texnomart-logo-removebg-preview.png"
-import {FcGoogle} from "react-icons/fc"
-import {FaApple} from "react-icons/fa"
+import { FcGoogle } from "react-icons/fc"
+import { FaApple } from "react-icons/fa"
+import showPwdImg from '../../Assets/img/show__password.svg'
+import hidePwdImg from '../../Assets/img/hide__password.svg';
+
 const Login = () => {
-  return (
-    <div className={c.login__wrapper}>
-        
-        <div className={c.login__logo__wrapper}>
-            <a href="/"><img src={logo} alt="" /></a>
-        </div>
-        <div className={c.login__form__wrapper}>
-            <div className={c.login__title}>
-                <p>Login to E-investment</p>
+    const [pwd, setPwd] = useState('');
+    const [isRevealPwd, setIsRevealPwd] = useState(false);
+    return (
+        <div className={c.login__wrapper}>
+
+            <div className={c.login__logo__wrapper}>
+                <a href="/"><img src={logo} alt="" /></a>
             </div>
-            <form action="" className={c.login__from}>
-                <input id='email' type="email" className={c.login__form__input__email} placeholder="type your email" required />
-                <input id='password' type="password" className={c.login__form__input__email} placeholder="type your password" required />
-                <button type='submit'  className={c.login__submit__btn}>Log in</button>
-                <p className={c.select__login__form__or}>or</p>
-                <a href="#" className={c.login__with__google}>
-                   <FcGoogle/>log in with  Google
-                </a>
-                <a href="#" className={c.login__with__google}>
-                   <FaApple/>log in with  Apple
-                </a>
-            </form>
+            <div className={c.login__form__wrapper}>
+                <div className={c.login__title}>
+                    <p>Login to E-investment</p>
+                </div>
+                <form action="" className={c.login__from}>
+                    <input id='email' type="email" className={c.login__form__input__email} placeholder="type your email" required />
+                    <div className={c.login__form__input__password__wrapper}>
+                        <input name="pwd"
+                            placeholder="Enter Password"
+                            type={isRevealPwd ? "text" : "password"}
+                            value={pwd}
+                            onChange={e => setPwd(e.target.value)} required />
+                        <img
+                            title={isRevealPwd ? "Hide password" : "Show password"}
+                            src={isRevealPwd ? hidePwdImg : showPwdImg}
+                            onClick={() => setIsRevealPwd(prevState => !prevState)}
+                        />
+                    </div>
+                    <button type='submit' className={c.login__submit__btn}>Log in</button>
+                    <p className={c.select__login__form__or}>or</p>
+                    <a href="#" className={c.login__with__google}>
+                        <FcGoogle />log in with  Google
+                    </a>
+                    <a href="#" className={c.login__with__google}>
+                        <FaApple />log in with  Apple
+                    </a>
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Login
