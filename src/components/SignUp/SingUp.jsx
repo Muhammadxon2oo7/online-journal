df04a303ad3cf224b22a9790f262642ab01b8f11
Muhammadxon_2oo7 <muhammadxon0709@gmail.com>
2023-03-22 23:02:11 +0300

    first commit

--- src/components/SignUp/SingUp.jsx
+++ src/components/SignUp/SingUp.jsx
@@ -1,35 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import c from './SigUp.module.scss'
 import logo from "../../Assets/img/texnomart-logo-removebg-preview.png"
-import {FcGoogle} from "react-icons/fc"
-import {FaApple} from "react-icons/fa"
+import { FcGoogle } from "react-icons/fc"
+import { FaApple } from "react-icons/fa"
+import showPwdImg from '../../Assets/img/show__password.svg'
+import hidePwdImg from '../../Assets/img/hide__password.svg';
+
 const SingUp = () => {
-  return (
-    <div className={c.signup__wrapper}>
-        <div className={c.signup__logo__wrapper}>
-            <a href="/"><img src={logo} alt="" /></a>
-        </div>
-        <div className={c.signup__form__wrapper}>
-            <div className={c.signup__title}>
-                <p>Sign Up for your account</p>
+    const [pwd, setPwd] = useState('');
+    const [isRevealPwd, setIsRevealPwd] = useState(false);
+    const [pwdtwo, setPwdtwo] = useState('');
+    const [isRevealPwdtwo, setIsRevealPwdtwo] = useState(false);
+    return (
+        <div className={c.signup__wrapper}>
+            <div className={c.signup__logo__wrapper}>
+                <a href="/"><img src={logo} alt="" /></a>
             </div>
-            <form action="" className={c.signup__from}>
-                <input type="email" className={c.signup__form__input__email} placeholder="type your email" required />
-                <input type="password" className={c.signup__form__input__email} placeholder="type your password" required />
-                <input type="text" className={c.signup__form__input__email} placeholder="type your username" required />
-                
-                <button type='submit'  className={c.signup__submit__btn}>Sign Up</button>
-                <p className={c.select__signup__form__or}>or</p>
-                <a href="#" className={c.signup__with__google}>
-                   <FcGoogle/>Sign Up with  Google
-                </a>
-                <a href="#" className={c.signup__with__google}>
-                   <FaApple/>Sign Up with  Apple
-                </a>
-            </form>
+            <div className={c.signup__form__wrapper}>
+                <div className={c.signup__title}>
+                    <p>Sign Up for your account</p>
+                </div>
+                <form action="" className={c.signup__from}>
+                    <input type="email" className={c.signup__form__input__email} placeholder="type your email" required />
+                    <input type="text" className={c.signup__form__input__email} placeholder="type your username" required />
+                    <div className={c.signup__form__input__password__wrapper}>
+                        <input name="pwd"
+                            placeholder="Enter Password"
+                            type={isRevealPwd ? "text" : "password"}
+                            value={pwd}
+                            onChange={e => setPwd(e.target.value)} required /> 
+                        <img
+                            title={isRevealPwd ? "Hide password" : "Show password"}
+                            src={isRevealPwd ? hidePwdImg : showPwdImg}
+                            onClick={() => setIsRevealPwd(prevState => !prevState)}
+                        />
+                    </div>
+                    <div className={c.signup__form__input__password__wrapper}>
+                        <input name="pwd"
+                            placeholder="confirm your Password"
+                            type={isRevealPwdtwo ? "text" : "password"}
+                            value={pwdtwo}
+                            onChange={e => setPwdtwo(e.target.value)} required /> 
+                        <img
+                            title={isRevealPwdtwo ? "Hide password" : "Show password"}
+                            src={isRevealPwdtwo ? hidePwdImg : showPwdImg}
+                            onClick={() => setIsRevealPwdtwo(prevState => !prevState)}
+                        />
+                    </div>
+                    <button type='submit' className={c.signup__submit__btn}>Sign Up</button>
+                    <p className={c.select__signup__form__or}>or</p>
+                    <a href="#" className={c.signup__with__google}>
+                        <FcGoogle />Sign Up with  Google
+                    </a>
+                    <a href="#" className={c.signup__with__google}>
+                        <FaApple />Sign Up with  Apple
+                    </a>
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default SingUp
